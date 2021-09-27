488af065548329e822410c75c8f4d7ffa40f9d9c
VladimirMolunov <vmolunov@gmail.com>
2021-09-27 11:56:55 +0300

    Leaves

--- draw2.py
+++ draw2.py
@@ -1,6 +1,7 @@
 import pygame
 import numpy as np
 from pygame.draw import *
+from pygame import transform
 
 pygame.init()
 
@@ -12,11 +13,12 @@ cgreen = (0, 104, 52)
 cpink = (255, 176, 129)
 cblack = (0, 0, 0)
 cwhite = (255, 255, 255)
+transparent = (200, 100, 90, 0)
 
 rect(screen, cpink, (0, 0, size, size))
 
-def leaf(xa, ya, xb, yb):
-    ellipse(screen, cgreen, (xa, ya, xb-xa, yb-ya))
+def leaf(surf, xa, ya, xb, yb):
+    ellipse(surf, cgreen, (xa, ya, xb, yb))
 
 def grect(xa, ya, width, height, x):
     polygon(screen, cgreen, [(xa, ya), (xa+width*np.cos(x), ya+width*np.sin(x)), (xa+width*np.cos(x)-height*np.sin(x), ya+width*np.sin(x)+height*np.cos(x)), (xa-height*np.sin(x), ya+height*np.cos(x))])
@@ -45,10 +47,19 @@ gap = 10
 
 grect(x1, y1, w1, h1, 0)
 grect(x1, y1-gap-h1, w1, h1, 0)
-grect(x1+w1-gap/2, y1-4*gap-2*h1, w1, h1+2*gap, np.pi/12)
-grect(x1+2*w1+2*gap, y1-6*gap-2.7*h1-5*gap, w1-gap/2, h1+4.5*gap, np.pi/8)
+grect(x1+w1+gap/2, y1-4*gap-2*h1, w1, h1+2*gap, np.pi/12)
+grect(x1+2*w1+3*gap, y1-6*gap-2.7*h1-5*gap, w1-gap/2, h1+4.5*gap, np.pi/8)
 
+surface = pygame.Surface((300,300), pygame.SRCALPHA)
+rect(surface, transparent, (0, 0, 300, 300))
+leaf(surface, 100,100,100,20)
+leaf(surface, 120,130,100,20)
+leaf(surface, 130,160,100,20)
+leaf(surface, 110,200,100,20)
+leaf(surface, 115,250,100,20)
+surface2 = pygame.transform.rotate(surface, 40)
 
+screen.blit(surface2, (35,50))
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
