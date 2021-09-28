bc7f6e6a54591b05466d51c9c1b38ebe500fb69a
VladimirMolunov <vmolunov@gmail.com>
2021-09-28 16:29:44 +0300

    Nothing really

--- Лаба 4/draw2.py
+++ Лаба 4/draw2.py
@@ -6,7 +6,7 @@ from pygame import transform
 pygame.init()
 
 FPS = 30
-size = 1000
+size = 800
 screen = pygame.display.set_mode((size, size))
 
 cgreen = (0, 104, 52)
@@ -26,24 +26,24 @@ def grect(xa, ya, width, height, x):
 def duga(xa, ya, xb, yb, angle1, angle2):
     arc(screen, cgreen, (xa, ya, xb-xa, yb-ya), angle1, angle2, 2)
 
-x1 = 500
-y1 = 600
-w1 = 30
-h1 = 120
+x1 = 400
+y1 = 580
+w1 = 24
+h1 = 96
 a1 = 0
-gap = 20
+gap = 16
 
 grect(x1, y1, w1, h1, 0)
 grect(x1, y1-gap-h1, w1, h1, 0)
 grect(x1+w1-gap/2, y1-gap-2*h1, w1, h1-gap, np.pi/12)
 grect(x1+2*w1+2*gap, y1-gap-2.7*h1-5*gap, w1-gap/2, h1+3.5*gap, np.pi/8)
 
-x1 = 300
-y1 = 700
-w1 = 15
-h1 = 60
+x1 = 240
+y1 = 660
+w1 = 12
+h1 = 48
 a1 = 0
-gap = 10
+gap = 8
 
 grect(x1, y1, w1, h1, 0)
 grect(x1, y1-gap-h1, w1, h1, 0)
@@ -52,20 +52,20 @@ grect(x1+2*w1+3*gap, y1-6*gap-2.7*h1-5*gap, w1-gap/2, h1+4.5*gap, np.pi/8)
 
 surface = pygame.Surface((300,300), pygame.SRCALPHA)
 rect(surface, transparent, (0, 0, 300, 300))
-leaf(surface, 100,100,100,20)
-leaf(surface, 120,130,100,20)
-leaf(surface, 130,160,100,20)
-leaf(surface, 110,200,100,20)
-leaf(surface, 115,250,100,20)
+leaf(surface, 80, 80, 80, 16)
+leaf(surface, 96, 104, 80, 16)
+leaf(surface, 104, 128, 80, 16)
+leaf(surface, 88, 160, 80, 16)
+leaf(surface, 92, 200, 80, 16)
 surface2 = pygame.transform.rotate(surface, 55)
 surface3 = pygame.transform.rotate(surface, -70)
 surface3 = pygame.transform.scale(surface3, (300,300))
 surface4 = pygame.transform.rotate(surface, 55)
 surface4 = pygame.transform.scale(surface4, (220,220))
 
-screen.blit(surface2, (35,50))
-screen.blit(surface3, (635,30))
-screen.blit(surface4, (120,370))
+screen.blit(surface2, (24,50))
+screen.blit(surface3, (458,164))
+screen.blit(surface4, (86,370))
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
