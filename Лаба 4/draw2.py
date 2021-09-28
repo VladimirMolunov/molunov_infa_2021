4036563f2011a900dcd3e0483b6afc89161c74ae
VladimirMolunov <vmolunov@gmail.com>
2021-09-28 16:47:03 +0300

    Теперь окно с рисунком нормально закрывается

--- Лаба 4/draw2.py
+++ Лаба 4/draw2.py
@@ -76,7 +76,7 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
-while True:
+while finished == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
