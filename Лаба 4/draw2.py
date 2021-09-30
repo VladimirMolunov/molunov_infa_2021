c803a4e45ca7afa79b81ba5000e713838c7eb5e2
VladimirMolunov <vmolunov@gmail.com>
2021-09-30 14:31:41 +0300

    Добавлена возможность двигать бамбук вверх и вниз; более точное размещение листьев

--- Лаба 4/draw2.py
+++ Лаба 4/draw2.py
@@ -31,25 +31,28 @@ def duga(xa, ya, xb, yb, angle1, angle2):
     arc(screen, cgreen, (xa, ya, xb-xa, yb-ya), angle1, angle2, 2)
 
 rightmove = 0
+rightup = -40
 x1 = size / 200 * 100 + rightmove
-y1 = size / 200 * 150
+y1 = size / 200 * 150 + rightup
 w1 = size / 200 * 6
 h1 = size / 200 * 24
 a1 = 0
 gap = size / 200 * 4
+initial = size / 200 * 8
 leftmove = 80
+leftup = -40
 
-grect(x1, y1, w1, h1, 0)
-grect(x1, y1 - gap - h1, w1, h1, 0)
-grect(x1 + w1 - gap / 2, y1 - gap - 2 * h1, w1, h1 - gap, np.pi / 12)
-grect(x1 + 2 * w1 + 2 * gap, y1 - gap - 2.7 * h1 - 5 * gap, w1 - gap / 2, h1 + 3.5 * gap, np.pi / 8)
+grect(x1, y1 + initial, w1, h1 + initial, 0)
+grect(x1, y1 - gap - h1, w1, h1 + initial, 0)
+grect(x1 + w1 + 3 * gap / 4, y1 + gap - 3 * h1, w1, h1 + 3 * gap, np.pi / 12)
+grect(x1 + 2 * w1 + 3.4 * gap, y1 - gap - 3.4 * h1 - 5 * gap, w1 - gap / 2, h1 + 3.5 * gap, np.pi / 8)
 
 x1 = size / 200 * 60 - leftmove
-y1 = size / 200 * 165
+y1 = size / 200 * 165 + leftup
 w1 = size / 200 * 3
 h1 = size / 200 * 12
 gap = size / 200 * 2
-initial = 35
+initial = size / 200 * 9
 leaflength = size / 8
 leafhight = size / 200 * 4
 
@@ -61,16 +64,16 @@ grect(x1 + 2 * w1 + 3 * gap, y1 - 6 * gap - 2.7 * h1 - 5 * gap, w1 - gap / 2, h1
 surface = pygame.Surface((int(size / 8 * 3), int(size / 8 * 3)), pygame.SRCALPHA)
 rect(surface, transparent, (0, 0, int(size / 8 * 3), int(size / 8 * 3)))
 leaf(surface, size / 200 * 20, size / 200 * 20, leaflength, leafhight)
-leaf(surface, size / 200 * 24, size / 200 * 28, leaflength, leafhight)
-leaf(surface, size / 200 * 26, size / 200 * 36, leaflength, leafhight)
-leaf(surface, size / 200 * 22, size / 200 * 44, leaflength, leafhight)
-leaf(surface, size / 200 * 23, size / 200 * 58, leaflength, leafhight)
+leaf(surface, size / 200 * 20, size / 200 * 28, leaflength, leafhight)
+leaf(surface, size / 200 * 22, size / 200 * 36, leaflength, leafhight)
+leaf(surface, size / 200 * 23, size / 200 * 44, leaflength, leafhight)
+leaf(surface, size / 200 * 20, size / 200 * 56, leaflength, leafhight)
 
-surface2 = pygame.transform.rotate(surface, 55)
-surface3 = pygame.transform.rotate(surface, -70)
+surface2 = pygame.transform.rotate(surface, 75)
+surface3 = pygame.transform.rotate(surface, -75)
 surface3 = pygame.transform.scale(surface3, (int(size / 8 * 3), int(size / 8 * 3)))
-screen.blit(surface2, (size / 800 * 44 + rightmove, size / 800 * 50))
-screen.blit(surface3, (size / 800 * 456 + rightmove, size / 800 * 164))
+screen.blit(surface2, (size / 800 * 24 + rightmove, size / 800 * 50 + rightup))
+screen.blit(surface3, (size / 800 * 506 + rightmove, size / 800 * 164 + rightup))
 
 threeleaves = pygame.Surface((int(size / 8 * 3), int(size / 8 * 3)), pygame.SRCALPHA)
 rect(threeleaves, transparent, (0, 0, int(size / 8 * 3), int(size / 8 * 3)))
@@ -80,7 +83,10 @@ leaf(threeleaves, size / 200 * 20, size / 200 * 40, leaflength, leafhight)
 
 threeleaves1 = pygame.transform.rotate(threeleaves, -70)
 threeleaves1 = pygame.transform.scale(threeleaves1, (int(size / 32 * 11), int(size / 32 * 11)))
-screen.blit(threeleaves1, (size / 200 * 114 + rightmove, size / 200 * 85))
+threeleaves2 = pygame.transform.rotate(threeleaves, 250)
+threeleaves2 = pygame.transform.scale(threeleaves2, (int(size / 32 * 13), int(size / 32 * 13)))
+screen.blit(threeleaves1, (size / 200 * 104 + rightmove, size / 200 * 85 + rightup))
+screen.blit(threeleaves2, (size / 200 * 19 + rightmove, size / 200 * 98 + rightup))
 
 leaflength = size / 6
 
@@ -102,15 +108,15 @@ fiveleaves1 = pygame.transform.rotate(fiveleaves, -80)
 fiveleaves1 = pygame.transform.scale(fiveleaves1, (int(size / 16 * 3), int(size / 16 * 3)))
 fiveleaves2 = pygame.transform.rotate(fiveleaves, 255)
 fiveleaves2 = pygame.transform.scale(fiveleaves2, (int(size / 14 * 3), int(size / 14 * 3)))
-screen.blit(fiveleaves1, (size / 800 * 282 - leftmove, size / 800 * 410))
-screen.blit(fiveleaves2, (size / 800 * 92 - leftmove, size / 800 * 415))
+screen.blit(fiveleaves1, (size / 800 * 282 - leftmove, size / 800 * 410 + leftup))
+screen.blit(fiveleaves2, (size / 800 * 92 - leftmove, size / 800 * 415 + leftup))
 
 threemoreleaves1 = pygame.transform.rotate(threemoreleaves, -80)
 threemoreleaves1 = pygame.transform.scale(threemoreleaves1, (int(size / 16 * 3), int(size / 16 * 3)))
 threemoreleaves2 = pygame.transform.rotate(threemoreleaves, 80)
 threemoreleaves2 = pygame.transform.scale(threemoreleaves2, (int(size / 16 * 3), int(size / 16 * 3)))
-screen.blit(threemoreleaves1, (size / 200 * 54 - leftmove, size / 200 * 132))
-screen.blit(threemoreleaves2, (size / 200 * 31 - leftmove, size / 200 * 139))
+screen.blit(threemoreleaves1, (size / 200 * 54 - leftmove, size / 200 * 132 + leftup))
+screen.blit(threemoreleaves2, (size / 200 * 31 - leftmove, size / 200 * 139 + leftup))
 
 pygame.display.update()
 clock = pygame.time.Clock()
