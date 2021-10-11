0e1e3499d28bd9092de9513b8b0c7eebf2cb1247
VladimirMolunov <vmolunov@gmail.com>
2021-10-11 21:35:41 +0300

    Небольшие исправления и улучшения

--- Лаба 6/main.py
+++ Лаба 6/main.py
@@ -6,12 +6,15 @@ pygame.init()
 TPS = 30
 dt = float(1/TPS)
 TicksPerFrame = 15
-screen = pygame.display.set_mode((1200, 900))
+(screen_width, screen_height) = (1200, 750)
+screen = pygame.display.set_mode((screen_width, screen_height))
 score = 0
 (x, y, r) = (-2, -2, 0)
 max_number_of_balls = 20
 max_radius = 100
 max_speed = 160
+min_speed = 10
+
 
 class Ball:
     def __init__(self, initialised, status, x, y, r, vx, vy, color, surface):
@@ -44,7 +47,7 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 transparent = (200, 200, 200, 0)
-darkblue = (0, 50, 80)
+background = (0, 50, 80)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 ball_list = []
@@ -62,9 +65,9 @@ def new_ball(balllist):
     Добавляет его параметры в список balllist
     """
     global x, y, r, surface_list
-    x = randint(100, 1100)
-    y = randint(100, 900)
-    r = randint(10, max_radius)
+    x = randint(max_radius, screen_width - max_radius)
+    y = randint(max_radius, screen_height - max_radius)
+    r = randint(min_speed, max_radius)
     vx = randint(-1 * max_speed, max_speed)
     vy = randint(-1 * max_speed, max_speed)
     color = COLORS[randint(0, 5)]
@@ -84,7 +87,8 @@ def inside(position, x, y, r):
     x, y - координаты центра шарика
     r - радиус шарика
     """
-    return True if ((position[0]-x)**2 + (position[1]-y)**2 <= r**2) else False
+    return True if ((position[0] - x) ** 2 + (position[1] - y)**2 <= r ** 2) else False
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -94,24 +98,22 @@ tickcount = 0
 while not finished:
     clock.tick(TPS)
     tickcount += 1
-    screen.fill(darkblue)
+    screen.fill(background)
     for ball in ball_list:
         ball.moveball(ball.vx, ball.vy, dt)
         if ball.status is True:
-            screen.blit(ball.surface, (ball.x, ball.y))
+            screen.blit(ball.surface, (ball.x - max_radius, ball.y - max_radius))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            ball_list.reverse()
-            for ball in ball_list:
-                is_inside = inside(event.pos, ball.x + 100, ball.y + 100, ball.r)
+            for ball in reversed(ball_list):
+                is_inside = inside(event.pos, ball.x, ball.y, ball.r)
                 if is_inside:
                     if ball.status:
                         score += 1
                         ball.status = False
                         break
-            ball_list.reverse()
     if tickcount % TicksPerFrame == 0:
         tickcount = 0
         new_ball(ball_list)
