905293ad60fc04476fe8095438bc2f53982afc15
VladimirMolunov <vmolunov@gmail.com>
2021-10-11 12:04:28 +0300

    Шарики нормально летают

--- Лаба 6/main.py
+++ Лаба 6/main.py
@@ -9,7 +9,7 @@ TicksPerFrame = 15
 screen = pygame.display.set_mode((1200, 900))
 score = 0
 (x, y, r) = (-2, -2, 0)
-max_number_of_balls = 4
+max_number_of_balls = 11
 max_radius = 100
 max_speed = 100
 
@@ -43,13 +43,14 @@ GREEN = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
-transparent = (200, 100, 90, 0)
+transparent = (200, 200, 200, 0)
+darkblue = (0, 50, 80)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 ball_list = []
 surface_list = []
 for i in range(0, max_number_of_balls + 1, 1):
-    surface_list.append(pygame.Surface((2 * max_radius, 2 * max_radius)))
+    surface_list.append(pygame.Surface((2 * max_radius, 2 * max_radius), pygame.SRCALPHA))
     surface_list[-1].fill(transparent)
     ball_list.append(Ball(False, True, 0, 0, 0, 0, 0, BLACK, surface_list[i]))
     # x, y, радиус, скорость x, скорость y
@@ -68,7 +69,7 @@ def new_ball(balllist):
     vy = randint(-1 * max_speed, max_speed)
     color = COLORS[randint(0, 5)]
     surface_list.pop(0)
-    surface_list.append(pygame.Surface((2 * max_radius, 2 * max_radius)))
+    surface_list.append(pygame.Surface((2 * max_radius, 2 * max_radius), pygame.SRCALPHA))
     balllist.append(Ball(True, True, x, y, r, vx, vy, color, surface_list[-1]))
     balllist.pop(0)
     ball = balllist[-1]
@@ -93,9 +94,11 @@ tickcount = 0
 while not finished:
     clock.tick(TPS)
     tickcount += 1
+    screen.fill(darkblue)
     for ball in ball_list:
         ball.moveball(ball.vx, ball.vy, dt)
-        screen.blit(ball.surface, (ball.x, ball.y))
+        if ball.status is True:
+            screen.blit(ball.surface, (ball.x, ball.y))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
