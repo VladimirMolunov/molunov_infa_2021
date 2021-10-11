8a88e3280582d0194ddd73432312f3bf943670e6
VladimirMolunov <vmolunov@gmail.com>
2021-10-11 11:28:54 +0300

    Полностью задан класс шарика

--- Лаба 6/main.py
+++ Лаба 6/main.py
@@ -4,6 +4,7 @@ from random import randint
 pygame.init()
 
 TPS = 30
+dt = float(1/TPS)
 TicksPerFrame = 15
 screen = pygame.display.set_mode((1200, 900))
 score = 0
@@ -12,18 +13,26 @@ max_number_of_balls = 4
 
 
 class Ball:
-    def __init__(self, status):
+    def __init__(self, initialised, status, x, y, r, vx, vy, color):
         self.status = status
+        self.initialised = initialised
+        self.x = x
+        self.y = y
+        self.r = r
+        self.vx = vx
+        self.vy = vy
+        self.color = color
 
-    def moveball(self, x, y):
-        self[0] += x
-        self[1] += y
+    def moveball(self, velx, vely, dt):
+        """
+        Двигает шарик
+        :param velx: скорость по x в секунду
+        :param vely: скорость по y в секунду
+        :param dt: FPS экрана
+        """
+        self.x += velx * dt
+        self.y += vely * dt
 
-
-ball_list = []
-for i in range(0, max_number_of_balls + 1, 1):
-    ball_list.append([0, 0, 0])
-    ball_list[i] = Ball(True)
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -33,6 +42,10 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+ball_list = []
+for i in range(0, max_number_of_balls + 1, 1):
+    ball_list.append(Ball(False, True, 0, 0, 0, 0, 0, BLACK))  # x, y, радиус, скорость x, скорость y
 
 
 def new_ball(balllist):
@@ -44,12 +57,11 @@ def new_ball(balllist):
     x = randint(100, 1100)
     y = randint(100, 900)
     r = randint(10, 100)
+    vx = randint(10, 100)
+    vy = randint(10, 100)
     color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
-    balllist.append([x, y, r])
-    balllist[-1] = Ball(True)
-    if balllist[max_number_of_balls] != [0, 0, 0]:
-        balllist.pop(0)
+    balllist.append(Ball(True, True, x, y, r, vx, vy, color))
+    balllist.pop(0)
 
 
 def inside(position, x, y, r):
@@ -70,12 +82,15 @@ tickcount = 0
 while not finished:
     clock.tick(TPS)
     tickcount += 1
+    for ball in ball_list:
+        circle(screen, ball.color, (int(ball.x), int(ball.y)), int(ball.r))
+        ball.moveball(ball.vx, ball.vy, dt)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             for ball in ball_list:
-                is_inside = inside(event.pos, x, y, r)
+                is_inside = inside(event.pos, ball.x, ball.y, ball.r)
                 if is_inside:
                     if ball.status is True:
                         score += 1
@@ -83,8 +98,7 @@ while not finished:
     if tickcount % TicksPerFrame == 0:
         tickcount = 0
         new_ball(ball_list)
-        pygame.display.update()
-        screen.fill(BLACK)
+    pygame.display.update()
 
 pygame.quit()
 print("Well done, your score is", score)
